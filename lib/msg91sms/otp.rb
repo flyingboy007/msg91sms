b1f7f8ed68141d9b5740ba4a1e3661ade86c18fc
Abhilash <abhilashvr007@gmail.com>
2017-06-24 01:58:21 +0530

    Revert "added otp verification"
    This reverts commit 72190817d1c540a8c89b2b2c00729c527ac4168f.

--- lib/msg91sms/otp.rb
+++ lib/msg91sms/otp.rb
@@ -4,26 +4,11 @@ module Msg91sms
   require 'json'
   class Otp
     def self.send_otp(sender, country_code, mobiles, message, otp)
+      require 'uri'
+      require 'net/http'
       mobile=country_code+mobiles
       #if no otp provided use url without otp and message parameters
       url=(otp==nil)?"https://control.msg91.com/api/sendotp.php?authkey=#{Msg91sms.configuration.authkey}&mobile=#{mobile}&sender=#{sender}":"https://control.msg91.com/api/sendotp.php?authkey=#{Msg91sms.configuration.authkey}&mobile=#{mobile}&&message=#{message}&otp=#{otp}&sender=#{sender}"
-      url = URI(url)
-
-      http = Net::HTTP.new(url.host, url.port)
-      http.use_ssl = true
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      request = Net::HTTP::Post.new(url)
-      request["cache-control"] = 'no-cache'
-
-      response = http.request(request)
-      JSON.parse(response.body)
-    end
-
-    def self.verify_otp(country_code, mobile, otp)
-      mobile=country_code+mobile
-      #if no otp provided use url without otp and message parameters
-      url="https://control.msg91.com/api/verifyRequestOTP.php?authkey=#{Msg91sms.configuration.authkey}&mobile=#{mobile}&otp=#{otp}"
       url = URI(url)
 
       http = Net::HTTP.new(url.host, url.port)
